90238eafdc65160d76ccc2df597e39df66b41617
limiteinductive <benjamintrom@gmail.com>
2022-08-25 19:31:42 +0200

    nteha

--- sdc/cli.py
+++ sdc/cli.py
@@ -20,12 +20,12 @@ def main():
     checkpoint_path = Path("~/.cache/stable-diffusion/stable-diffusion.pt")
     if not checkpoint_path.exists():
         typer.echo(f"Downloading stable-diffusion checkpoint to {checkpoint_path}")
-        subprocess.run(["wget", "-P", "~/.cache/stable-diffusion/", "https://storage.googleapis.com/laion_limiteinducive/stable-diffusion.pt"])
+        subprocess.run(["wget", "-P", f"{Path.home() / '.cache/stable-diffusion/'}", "https://storage.googleapis.com/laion_limiteinducive/stable-diffusion.pt"])
 
     output = run_stable_diffusion(prompt=prompt, batch_size=int(batch_size), device="cuda", checkpoint_path=checkpoint_path)
 
-    path = Path("./.outputs")
-    path.mkdir(exist_ok=True)
+    output_path = Path("./outputs")
+    output_path.mkdir(exist_ok=True)
     for i, image in enumerate(output["sample"]):
-        image.save(Path("./outputs/") / f"{i}.png")
+        image.save(output_path / f"{str(i).zfill(4)}.png")
 
