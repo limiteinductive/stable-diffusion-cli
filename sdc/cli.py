470ceef72377d0b9b60fbd8ed6af2b49fa27f551
limiteinductive <benjamintrom@gmail.com>
2022-08-25 19:38:39 +0200

    nteah

--- sdc/cli.py
+++ sdc/cli.py
@@ -17,13 +17,13 @@ def main():
 
     batch_size = Prompt.ask(":sushi: How many images", default=1)
 
-    checkpoint_path = Path("~/.cache/stable-diffusion/stable-diffusion.pt")
-    if not checkpoint_path.exists():
+    checkpoint_path = Path.home() / ".cache/stable-diffusion/"
+    checkpoint_path.mkdir(exist_ok=True)
+    if not (checkpoint_path / "stable-diffusion.pt").exists():
         typer.echo(f"Downloading stable-diffusion checkpoint to {checkpoint_path}")
         subprocess.run(["wget", "-P", f"{Path.home() / '.cache/stable-diffusion/'}", "https://storage.googleapis.com/laion_limiteinducive/stable-diffusion.pt"])
 
-    output = run_stable_diffusion(prompt=prompt, batch_size=int(batch_size), device="cuda", checkpoint_path=checkpoint_path)
-
+    output = run_stable_diffusion(prompt=prompt, batch_size=int(batch_size), device="cuda", checkpoint_path=checkpoint_path/"stable-diffusion.pt")
     output_path = Path("./outputs")
     output_path.mkdir(exist_ok=True)
     for i, image in enumerate(output["sample"]):
